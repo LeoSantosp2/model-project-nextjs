a3816a7f1d33e4a711863f4c930e14184b14feeb
Leonardo Santos <leogilberto2010@hotmail.com>
2023-06-16 18:57:16 -0300

    Adicionando e configurando o Styled Component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,14 @@
 import { AppProps } from 'next/app';
+import { ThemeProvider } from 'styled-components';
+
+import { GlobalStyles } from '../src/styles/global-styles';
+import { theme } from '../src/styles/theme';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
---
+++ src/styles/theme.ts
@@ -0,0 +1,6 @@
+export const theme = {
+  colors: {
+    primary: '#F2F2F2',
+    secondary: '#000000',
+  },
+};

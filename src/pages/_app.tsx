c24eaf542672c930eae586434679357fcf905f8d
Leonardo Santos <leogilberto2010@hotmail.com>
2023-06-16 18:58:53 -0300

    Trocado a pasta pages de local

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,8 @@
 import { AppProps } from 'next/app';
 import { ThemeProvider } from 'styled-components';
 
-import { GlobalStyles } from '../src/styles/global-styles';
-import { theme } from '../src/styles/theme';
+import { GlobalStyles } from '../styles/global-styles';
+import { theme } from '../styles/theme';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (

bc364e5094b4899eba92594054c501b7fbd05bb5
Leonardo Santos <92178094+LeoSantosp2@users.noreply.github.com>
2025-10-02 18:48:54 -0300

    Retirado o Styled Components e, atualizado pacotes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,12 @@
-'use client';
-
-import { ThemeProvider } from 'styled-components';
-
-import { GlobalStyles } from '../styles/global-styles';
-import { theme } from '../styles/theme';
-
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyles />
-      <html lang="pt-BR">
-        <title>Model Project NextJs</title>
-        <body>{children}</body>
-      </html>
-    </ThemeProvider>
+    <html lang="pt-BR">
+      <title>Model Project NextJs</title>
+      <body>{children}</body>
+    </html>
   );
 }
